cf8c04e8e629a1837508954dbfce59bcd9f60313
WangLangJing <932444208@qq.com>
2017-07-06 17:08:32 +0800

    添加RowBase类

--- ConsoleTestUnit/Program.cs
+++ ConsoleTestUnit/Program.cs
@@ -8,34 +8,52 @@ using System.Diagnostics;
 using Handiness.Metadata;
 namespace ConsoleTestUnit
 {
-    class Student
-    {
-        public String Name { get; set; }
-        public Int32 Age { get; set; }
-    }
 
+    public class Student : RowBase
+    {
+        public String Name
+        {
+            get => this._name;
+            set
+            {
+                if (this._name != value)
+                {
+                    this._name = value;
+                    this.OnNotifyPropertyChanged(nameof(this.Name), value);
+                }
+            }
+        }
+        private String _name;
+    }
     class Program
     {
         static void Main(string[] args)
         {
             List<Student> query = new List<Student>();
-   
-            using (IMetadataProvider provider = MetadataProvider.GetMetadataProvider("#M1"))
-            {
-                var temp =MetadataProvider.GetMetadataProviders();
-                Console.WriteLine(temp.GetType().Namespace);
-                provider.Open("server=192.168.0.108;Port=3306;Uid=root;Pwd=123456;DataBase=handinessOrm;Pooling=true;charset=utf8;");
-                Stopwatch watch = new Stopwatch();
-                watch.Start();
-                IList<ColumnSchema> schemas = provider.GetColumnSchemas("personal");
-                watch.Stop();
-                Console.WriteLine("1:" + watch.ElapsedMilliseconds);
-                watch.Restart();
-                IList<ColumnSchema> schemas1 = provider.GetColumnSchemas("happy");
-                watch.Stop();
-                Console.WriteLine("2:" + watch.ElapsedMilliseconds);
-                Int32 i = 0;
-            }
+            Student student = new Student();
+            student.PropertyChanged += (s, p) =>
+              {
+                  Console.WriteLine(p.PropertyName + "Changed");
+              };
+            student.Name = "老师";
+            student.NotifyPropertyValue("Name", "学生");
+           Console.WriteLine(student.Name);
+            //using (IMetadataProvider provider = MetadataProvider.GetMetadataProvider("#M1"))
+            //{
+            //    var temp =MetadataProvider.GetMetadataProviders();
+            //    Console.WriteLine(temp.GetType().Namespace);
+            //    provider.Open("server=192.168.0.108;Port=3306;Uid=root;Pwd=123456;DataBase=handinessOrm;Pooling=true;charset=utf8;");
+            //    Stopwatch watch = new Stopwatch();
+            //    watch.Start();
+            //    IList<ColumnSchema> schemas = provider.GetColumnSchemas("personal");
+            //    watch.Stop();
+            //    Console.WriteLine("1:" + watch.ElapsedMilliseconds);
+            //    watch.Restart();
+            //    IList<ColumnSchema> schemas1 = provider.GetColumnSchemas("happy");
+            //    watch.Stop();
+            //    Console.WriteLine("2:" + watch.ElapsedMilliseconds);
+            //    Int32 i = 0;
+            //}
 
         }
     }
---
+++ Handiness/RowBase.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Reflection;
+namespace Handiness
+{
+    /*-------------------------------------------------------------------------
+ * 版权所有：王浪静
+ * 作者：王浪静
+ * 联系方式：932444208@qq.com
+ * 创建时间： 2017/7/6 15:31:31
+ * 版本号：v1.0
+ * .NET 版本：4.0
+ * 本类主要用途描述：此类是数据库每行记录所映射到类的实例的基类，
+ * 提供了一些基本的操作方法例如删除，更新，查询
+ *  -------------------------------------------------------------------------*/
+
+    public class RowBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 提供一个直接修改实例属性值的方法，此方法存在较大的性能损耗
+        /// </summary>
+        /// <param name="propertyName">属性名称</param>
+        /// <param name="newValue">新的值</param>
+        public void NotifyPropertyValue(String propertyName, Object newValue)
+        {
+            Type type = this.GetType();
+            PropertyInfo propertyInfo = type.GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance|BindingFlags.SetProperty);
+            if (propertyInfo != null)
+            {
+                propertyInfo.SetValue(this, newValue, null);
+            }
+        }
+        /// <summary>
+        /// 当前实例的属性值变跟记录
+        /// </summary>
+        protected Dictionary<String, Object> _notifyLog = new Dictionary<String, Object>();
+        protected RowStateType State { get; set; } = RowStateType.Normal;
+        /**************/
+        protected void OnNotifyPropertyChanged(
+            String proteryName,
+            Object newValue)
+        {
+            this.WriteNotifLog(proteryName, newValue);
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(proteryName));
+        }
+        protected void WriteNotifLog(String proteryName, Object newValue)
+        {
+            if (!this._notifyLog.ContainsKey(proteryName))
+            {
+                this._notifyLog.Add(proteryName, newValue);
+            }
+            else
+            {
+                this._notifyLog[proteryName] = newValue;
+            }
+        }
+        /// <summary>
+        /// 判断当前实例是否可用
+        /// </summary>
+        /// <returns></returns>
+        protected Boolean IsUsable()
+        {
+            return (this.State != RowStateType.Deleted);
+        }
+    }
+    /// <summary>
+    /// 当前行对象实例的状态
+    /// </summary>
+    public enum RowStateType
+    {
+        /// <summary>
+        /// 正常的
+        /// </summary>
+        Normal = 0,
+        /// <summary>
+        /// 表示此实例在数据库中已被删除，不应当继续使用
+        /// </summary>
+        Deleted = 1
+    }
+}
