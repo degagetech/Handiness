8f020831de6a2a0ef9bbc67c888e54bc6f29af9a
WangLangJing <932444208@qq.com>
2017-08-07 18:44:58 +0800

    添加Handiness 实体类生成VS插件项目

---
+++ Handiness.VSIX/TextResource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handiness.VSIX
+{
+    /*-------------------------------------------------------------------------
+ * 版权所有：王浪静
+ * 作者：王浪静
+ * 联系方式：932444208@qq.com
+ * 创建时间： 2017/8/7 17:43:05
+ * 版本号：v1.0
+ * .NET 版本：4.6
+ * 本类主要用途描述：
+ *  -------------------------------------------------------------------------*/
+    public class TextResource
+    {
+        /// <summary>
+        /// 命令标题
+        /// </summary>
+        public const String CommandTitle = "实体类生成";
+    }
+}
